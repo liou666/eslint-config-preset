f73727ca7254fb5510e0e0875d640d7f875731ba
liou <liou666@126.com>
2022-11-05 17:11:06 +0800

    feat: update react lint rule

--- packages/react/index.js
+++ packages/react/index.js
@@ -23,6 +23,34 @@ module.exports = {
     'react/jsx-uses-react': 'off',
     'react/react-in-jsx-scope': 'off',
 
+    'react/jsx-boolean-value': 'error',
+
+    'react/sort-prop-types': 'error',
+    'react/sort-comp': 'error',
+
+    // code style
+    'jsx-quotes': ['error', 'prefer-single'],
+    'react/jsx-indent-props': [2, 2],
+    'react/jsx-indent': [2, 2, { indentLogicalExpressions: true }],
+    'react/jsx-first-prop-new-line': 'error',
+    'react/jsx-newline': ['error', { prevent: true }],
+    'react/jsx-closing-tag-location': 'error',
+    'react/jsx-one-expression-per-line': ['error', { allow: 'single-child' }],
+    'react/jsx-wrap-multilines': ['error', {
+      declaration: 'parens-new-line',
+      assignment: 'parens',
+      return: 'parens-new-line',
+      arrow: 'parens',
+      condition: 'ignore',
+      logical: 'ignore',
+      prop: 'ignore',
+    }],
+    'react/jsx-tag-spacing': ['error', {
+      closingSlash: 'never',
+      // "beforeSelfClosing": "allow",
+      afterOpening: 'never',
+      beforeClosing: 'never',
+    }],
     'react/self-closing-comp': [
       'error',
       {
@@ -30,8 +58,11 @@ module.exports = {
         html: true,
       },
     ],
-    'react/jsx-boolean-value': 'error',
-    'react/jsx-tag-spacing': 'error',
+    'react/jsx-no-constructed-context-values': 'error',
+    'react/jsx-closing-bracket-location': ['error'],
+    'react/jsx-max-props-per-line': ['error', { maximum: 2 }],
+    'react/void-dom-elements-no-children': 'error',
+
     // 'react-hooks/rules-of-hooks': 'error',
     // 'react-hooks/exhaustive-deps': 'error',
 
