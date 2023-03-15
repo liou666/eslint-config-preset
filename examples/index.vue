584f4021d6535aad26ab3c12c1d7b9ca66a4ef97
liou <liou666@126.com>
2023-03-15 23:57:51 +0800

    feat: vue lint

---
+++ examples/index.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { defineProps, ref } from 'vue'
+const props = defineProps({
+  msg: String,
+})
+const count = ref(0)
+</script>
+
+<template lang='ts'>
+  <h1>{{ msg }}</h1>
+  <button>+</button>
+</template>
+
